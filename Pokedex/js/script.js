7f076e61bc051a926b6331dc8c459c6ddd84357f
vicentegm2 <vicente280228@gmail.com>
2023-09-07 08:46:52 +0200

    Paginación v.1

--- Pokedex/js/script.js
+++ Pokedex/js/script.js
@@ -1,5 +1,8 @@
 const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
+const pokemonPorPagina = 20;
+let paginaActual = 1;
+
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for (let i = 1; i <= 151; i++) {
@@ -10,20 +13,25 @@ for (let i = 1; i <= 151; i++) {
 
 function mostrarPokemon(poke) {
 
-    let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
-    tipos = tipos.join('');
+    const inicio = (paginaActual - 1) * pokemonPorPagina;
+    const fin = paginaActual * pokemonPorPagina;
 
-    let pokeId = poke.id.toString();
-    if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
-    } else if (pokeId.length === 2) {
-        pokeId = "0" + pokeId;
-    }
+    if (poke.id >= inicio + 1 && poke.id <= fin) {
+
+        let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+        tipos = tipos.join('');
+
+        let pokeId = poke.id.toString();
+        if (pokeId.length === 1) {
+            pokeId = "00" + pokeId;
+        } else if (pokeId.length === 2) {
+            pokeId = "0" + pokeId;
+        }
 
 
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    div.innerHTML = `
+        const div = document.createElement("div");
+        div.classList.add("pokemon");
+        div.innerHTML = `
         <p class="pokemon-id-back">#${pokeId}</p>
         <div class="pokemon-imagen">
             <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
@@ -42,7 +50,11 @@ function mostrarPokemon(poke) {
             </div>
         </div>
     `;
-    listaPokemon.append(div);
+        listaPokemon.append(div);
+    }
+
+
+
 }
 
 botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
@@ -68,3 +80,41 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
     }
 }))
 
+// ...
+
+const prevPageBtn = document.getElementById("prev-page");
+const nextPageBtn = document.getElementById("next-page");
+const pageNum = document.getElementById("page-num");
+
+prevPageBtn.addEventListener("click", () => {
+    if (paginaActual > 1) {
+        paginaActual--;
+        actualizarPagina();
+    }
+});
+
+nextPageBtn.addEventListener("click", () => {
+    const totalPages = Math.ceil(151 / pokemonPorPagina);
+    if (paginaActual < totalPages) {
+        paginaActual++;
+        actualizarPagina();
+    }
+});
+
+function actualizarPagina() {
+    listaPokemon.innerHTML = "";
+    const inicio = (paginaActual - 1) * pokemonPorPagina;
+    const fin = paginaActual * pokemonPorPagina;
+
+    for (let i = inicio + 1; i <= fin; i++) {
+        if (i <= 151) {
+            fetch(URL + i)
+                .then((response) => response.json())
+                .then(data => mostrarPokemon(data))
+        }
+    }
+    pageNum.textContent = `Página ${paginaActual}`;
+}
+
+// Llama a actualizarPagina al inicio para mostrar la primera página
+actualizarPagina();
